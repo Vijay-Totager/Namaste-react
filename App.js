420abc6e59c0a6334d84096e3cac6ae254423329
Vijayakumar-123 <vijayakumartotager@gmail.com>
2024-08-08 11:39:23 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading=React.createElement("h1",{id:'hello'},[React.createElement('h2',{},'Namaste react'),React.createElement('h2',{},'Namaste react')])
+
+
+
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)    
